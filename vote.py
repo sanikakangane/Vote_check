38a3182d924802f0adeda4f619f2d61b306fcbba
Sanika Kangane <sanikakangane166@gmail.com>
2026-07-08 21:37:34 +0530

    Update current year calculation to use datetime

--- vote.py
+++ vote.py
@@ -1,11 +1,11 @@
 DOB = int(input("Enter your Birth year: "))
 
-Current_year = 2025
+Current_year = datetime.now().year
 age = Current_year - DOB
 
 print("Your age is:", age)
 
 if age >= 18:
     print("Hey! You are eligible to vote.")
-else: 
+else:
     print("Hey! You are NOT eligible to vote.")
